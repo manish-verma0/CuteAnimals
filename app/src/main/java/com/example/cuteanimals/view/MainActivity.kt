eb28cb7e7d5310d60d2db61b1613ebcba4cc39ef
manish.v <vermamv1881@gmail.com>
2024-06-22 23:32:29 +0530

    added dependencies

--- app/src/main/java/com/example/cuteanimals/view/MainActivity.kt
+++ app/src/main/java/com/example/cuteanimals/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.cuteanimals
+package com.example.cuteanimals.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cuteanimals.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
